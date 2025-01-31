dba47023847cf5b65e6adfbcae730a3b73fe5977
angel troncoso <angeltroncoso2019@outlook.es>
2025-01-31 10:19:16 -0300

    incorporo cdn a app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :contacts
+  get "pages/home"
+  get "pages/terms"
+  get "pages/faq"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +13,6 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+   # Defines the root path route ("/")
+   root "pages#home"
 end
